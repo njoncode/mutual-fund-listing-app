abbe1f2803f7f829b423a13694693624d977f75f
njoncode <njoncode@gmail.com>
2021-10-07 20:29:08 +0530

    Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
-
+import Gravatar  from 'react-gravatar';
+import { Link } from 'react-router-dom';
 
 import '../styles/header.scss';
+
+import mfImage from '../images/mutual-fund.png';
 
 const Header = () => {
 
   return (
     <div className="header-container">
-        <p>SignIn</p>
+      <img className='mf-image' src={mfImage} alt='mutual-find-logo'/>
+      <div className='header-right-content'>
+        <Gravatar email='' size={40}/>
+        <Link className='link-sign-in' to='/sign-in'>SignIn</Link>
+      </div>
     </div>
   );
 };
